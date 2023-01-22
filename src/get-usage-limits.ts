a494584233539eb6d3cc2f8f243e8327400735ac
j-mendez <jeff@a11ywatch.com>
2023-01-22 10:50:19 -0500

    chore(price): add monthly price schedule

--- src/get-usage-limits.ts
+++ src/get-usage-limits.ts
@@ -1,52 +1,67 @@
+export const multiplier = (n: number, x: number = 30) => n * x
+
+const BASE = multiplier(10000, 6)
+
+export const BASE_L1 = multiplier(300000)
+export const BASE_L2 = multiplier(600000)
+export const BASE_L3 = multiplier(1400000)
+export const BASE_L4 = multiplier(2000000)
+export const BASE_L5 = multiplier(4000000)
+export const BASE_H1 = multiplier(5000000)
+export const BASE_H2 = multiplier(10000000)
+export const BASE_H3 = multiplier(20000000)
+export const BASE_H4 = multiplier(35000000)
+export const BASE_H5 = multiplier(60000000)
+
 // determine plan usage by role limits to ms, used across multiple services. The values are static for transparency
 export const getUsageLimitsMs = (role: number): number => {
     let usage = 0
   
     switch (role) {
       case 0: {
-        usage = 10000
+        usage = BASE
         break
       }
       // normal plans
       case 1: {
-        usage = 300000
+        usage = BASE_L1
         break
       }
       case 2: {
-        usage = 600000
+        usage = BASE_L2
         break
       }
       case 3: {
-        usage = 1400000
+        usage = BASE_L3
         break
       }
       case 4: {
-        usage = 2000000
+        usage = BASE_L4
         break
       }
       case 5: {
-        usage = 4000000
+        usage = BASE_L5
         break
       }
       // high tier plans
       case 6: {
-        usage = 5000000
+        usage = BASE_H1
         break
       }
       case 7: {
-        usage = 10000000
+        usage = BASE_H2
         break
       }
       case 8: {
-        usage = 20000000
+        usage = BASE_H3
         break
       }
       case 9: {
-        usage = 35000000
+        usage = BASE_H4
         break
       }
       case 10: {
-        usage = 60000000
+        usage = BASE_H5
         break
       }
       // account blocked!
@@ -55,10 +70,10 @@ export const getUsageLimitsMs = (role: number): number => {
         break
       }
       default: {
-        usage = 10000
+        usage = BASE
         break
       }
     }
-  
+   
     return usage
   }
--- src/price-config.ts
+++ src/price-config.ts
@@ -1,89 +1,93 @@
+import {multiplier, BASE_L1, BASE_L2, BASE_L3, BASE_L4, BASE_L5, BASE_H1, BASE_H2, BASE_H3,BASE_H4,BASE_H5} from './get-usage-limits'
+
 const feats = [
   '50 domains',
   'Wildcard subdomains and TLD',
   'Sitemap extended domain coverage',
   'All pages checked daily with email status reports',
+  'Unlimited email reports',
   'Analytics monitor',
-  '10,000+ AI Models to validate resources (text OCR, images, and etc)',
+  '10,000+ AI Models to validate resources',
   'PageSpeed Insights across all pages',
-  'Webhooks for audits',
-  'Custom workflows and events',
+  'Enterprise-grade infrastructure',
   'Unlimited API request',
 ]
+
+const formatter = typeof Intl.NumberFormat !== "undefined" ? new Intl.NumberFormat() : {format: (n: number) => n};
 
 const lPlans = [
   {
     title: 'L1',
-    details: ['300 seconds daily uptime'],
+    details: [`${formatter.format(BASE_L1 / 1000)} seconds monthly uptime`],
     cost: '$14/month',
     costYearly: '$12/month',
-    pageCount: 10000,
+    pageCount: multiplier(10000),
   },
   {
     title: 'L2',
-    details: ['600 seconds daily uptime'],
+    details: [`${formatter.format(BASE_L2 / 1000)} seconds monthly uptime`],
     cost: '$24/month',
     costYearly: '$20/month',
-    pageCount: 20000,
+    pageCount: multiplier(20000),
   },
   {
     title: 'L3',
-    details: ['1,400 seconds daily uptime'],
+    details: [`${formatter.format(BASE_L3 / 1000)} seconds monthly uptime`],
     cost: '$44/month',
     costYearly: '$37/month',
-    pageCount: 42000,
+    pageCount: multiplier(42000),
   },
   {
     title: 'L4',
-    details: ['2,000 seconds daily uptime'],
+    details: [`${formatter.format(BASE_L4 / 1000)} seconds monthly uptime`],
     cost: '$54/month',
     costYearly: '$45/month',
-    pageCount: 62000,
+    pageCount: multiplier(62000),
   },
   {
     title: 'L5',
-    details: ['4,000 seconds daily uptime'],
+    details: [`${formatter.format(BASE_L5 / 1000)} seconds monthly uptime`],
     cost: '$84/month',
     costYearly: '$70/month',
-    pageCount: 124000 ,
+    pageCount: multiplier(124000),
   },
 ]
 
 const hPlans = [
   {
     title: 'H1',
-    details: ['5,000 seconds daily uptime'],
+    details: [`${formatter.format(BASE_H1 / 1000)} seconds monthly uptime`],
     cost: '$94/month',
     costYearly: '$78/month',
-    pageCount: 155000,
+    pageCount: multiplier(155000),
   },
   {
     title: 'H2',
-    details: ['10,000 seconds daily uptime'],
+    details: [`${formatter.format(BASE_H2 / 1000)} seconds monthly uptime`],
     cost: '$134/month',
     costYearly: '$112/month',
-    pageCount: 310000,
+    pageCount: multiplier(310000),
   },
   {
     title: 'H3',
-    details: ['20,000 seconds daily uptime'],
+    details: [`${formatter.format(BASE_H3 / 1000)} seconds monthly uptime`],
     cost: '$194/month',
     costYearly: '$161/month',
-    pageCount: 620000,
+    pageCount: multiplier(620000),
   },
   {
     title: 'H4',
-    details: ['35,000 seconds daily uptime'],
+    details: [`${formatter.format(BASE_H4 / 1000)} seconds monthly uptime`],
     cost: '$334/month',
     costYearly: '$278/month',
-    pageCount: 1085000,
+    pageCount: multiplier(1085000),
   },
   {
     title: 'H5',
-    details: ['60,000 seconds daily uptime'],
+    details: [`${formatter.format(BASE_H5 / 1000)} seconds monthly uptime`],
     cost: '$544/month',
     costYearly: '$453/month',
-    pageCount: 1860000,
+    pageCount: multiplier(1860000),
   },
 ]
 
